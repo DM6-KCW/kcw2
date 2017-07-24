aec9d4c89b4c49270646a882db244f8ed3fdf646
juan Hernandez <juan.hernandeznoriega01@utrgv.edu>
2017-07-24 11:57:48 -0500

    add to sketeton

---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
+
+
+import "./Home.css";
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                home page
+            </div>
+        )
+    }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+    <BrowserRouter>
+        {/*<Provider store={ store }>*/}
+    <App />
+        {/*</Provider>*/}
+    </BrowserRouter>
+    , document.getElementById('root'));
+
 registerServiceWorker();
