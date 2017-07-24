dcfbfff58b7df572eb950562a420b664c8151b90
FinalFlashLight <ryan.godwin524@outlook.com>
2017-07-24 16:50:41 -0500

    collections started

---
+++ src/components/collections/collection/collection1/collection1.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+
+import "./collection1.css";
+
+class Collection1 extends Component {
+    render() {
+        return (
+            <div>
+                <div id="moonlight-text">
+                    Moonlight Collection
+                </div>
+                <div id="moonlight-date">
+                    Spring Summer 2017
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Collection1;
