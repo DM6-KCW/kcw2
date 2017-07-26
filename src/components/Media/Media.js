85a723e70cc8cc2ef1224d77d462b7f0dbdc494a
juan Hernandez <juan.hernandeznoriega01@utrgv.edu>
2017-07-25 21:25:56 -0500

    worked on media component

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -5,6 +5,7 @@ import React, {Component} from 'react';
 import "./Media.css";
 import image from "../../images/FB_IMG_1500476060183[125].jpg";
 import imageTwo from "../../images/FB_IMG_1500476073426[124].jpg";
+import imageThree from "../../images/FB_IMG_1500475775897[120].jpg";
 
 class Media extends Component {
 
@@ -18,33 +19,31 @@ class Media extends Component {
         return (
             <div id="media">
                 <div id="title">
-                    <h3 id="press">
+                    <h2 id="press">
                         PRESS
-                    </h3>
+                    </h2>
+                    <hr />
                 </div>
                 <div className="row">
-
-                    <figure id="figOne" className="col-sm-6">
-                        <img src={image} alt="article" height="500px" width="500px" id="image"/>
-                        <br/>
-                        <figurecaption>
-                            first column
-
-                            {/*{}*/}
-                        </figurecaption>
-                    </figure>
-
-
-                    <figure id="figTwo" className="col-sm-6">
-                            <img src={imageTwo} alt="artile" height="500px" width="500px" img="imageTwo"/>
-                            <br/>
-                            <figurecaption>
-                                second column
+                    <div className="col-xs-12 col-sm-6 col-md-6">
+                            <img src={image} alt="article" className="img-responsive" height="500px" width="500px" id="image"/>
 
                                 {/*{}*/}
-                            </figurecaption>
-                    </figure>
+                    </div>
+                    <div className="col-xs-12 col-sm-6 col-md-6">
+                            <div id="figTwo">
+                                    <img src={imageTwo} alt="artile" className="img-responsive" height="500px" width="500px" id="image" />
 
+                            </div>
+                    </div>
+                <div className="row">
+                    <div className="col-xs-12 col-sm-6 col-md-6">
+                        <div id="figThree">
+                            <img src={imageThree} alt="article" className="img-responsive" id="image" />
+
+                        </div>
+                    </div>
+                </div>
                 </div>
             </div>
 
