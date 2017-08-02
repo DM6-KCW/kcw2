808143b2c0c5c94baae4d0cff76ed1da541612b2
juan Hernandez <juan.hernandeznoriega01@utrgv.edu>
2017-08-01 22:23:56 -0500

    worked on stuff

---
+++ src/components/AdminPage/subAdminComponents/ViewOrders/ViewOrders.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+
+import './ViewOrders.css';
+
+class ViewOrders extends Component {
+
+    constructor() {
+        super();
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Orders Summary</h3>
+            </div>
+        )
+    }
+}
+
+export default ViewOrders;
